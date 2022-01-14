8dabb882616e6769cbcc6327f973449d7ad8f0ca
Jake Spitznagel <jspitzna@uwaterloo.ca>
2022-01-13 20:49:22 -0500

    added simple serve script

---
+++ serve.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.7
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
